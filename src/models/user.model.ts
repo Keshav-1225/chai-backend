8de8dc1982af833baf5cf48db0327daa1d10961b
keshav-1225 <keshaav.mishra2006@gmail.com>
2026-07-10 15:34:48 +0530

    Add user and video model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -58,19 +58,31 @@ userSchema.methods.isPasswordCorrect = async function (password: string) {
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function () {
     return jwt.sign(
-    {
-        _id : this._id,
-        email: this.email,
-        fullname: this.fullname,
-        username: this.username
-    },
-    process.env.ACCESS_TOKEN_SECRET!,
-    {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY as jwt.SignOptions["expiresIn"]
-    }
-)
+        {
+            _id: this._id,
+            email: this.email,
+            fullname: this.fullname,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET!,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY as jwt.SignOptions["expiresIn"]
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.ACCESS_TOKEN_SECRET!,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY as jwt.SignOptions["expiresIn"]
+        }
+    )
 }
 
 const User = mongoose.model("User", userSchema)
