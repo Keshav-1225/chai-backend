24b5669eea9268f795b9923c02593a80d1c9bef8
keshav-1225 <keshaav.mishra2006@gmail.com>
2026-07-15 20:45:17 +0530

    set up auth middleware | login | logout

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,21 @@
+import {Request, Response, NextFunction } from "express";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiErrors.js";
+import  jwt  from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req:Request, res:Response, next:NextFunction) => {
+    try {
+        const token = req.cookies?.accessToken ||req.header("Authorization")?.replace("Brearer ","")
+        if(!token) throw new ApiError(401,"Unauthorized request")
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!)
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+        if(!user) throw new ApiError(401,"Invalid access token")
+        req.user = user
+        next()  
+    } catch (error: any) {
+        throw new ApiError(401,error.message || "Invalid access token")
+    }
+})
